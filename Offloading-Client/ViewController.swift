8479921d73854e7827ed4f983136bbc2b0160c2e
girijah <girijahnagarajah@gmail.com>
2018-05-07 10:32:39 +0530

    Image recognition computation & UI added

--- Offloading-Client/ViewController.swift
+++ Offloading-Client/ViewController.swift
@@ -7,19 +7,82 @@
 //
 
 import UIKit
+import AVFoundation
+import Vision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    @IBOutlet weak var objectLabel: UILabel!
+    
+    private let endPoint = "http://192.168.43.41:8181/recognizeImage"
+    private let urlSession = URLSession.shared
+    
+    var switcher = 2   // 1 - Local Execution, 2 - Remote Execution
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupCaptureSession()
+        
+        view.addSubview(objectLabel)
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func setupCaptureSession() {
+        
+        // creates a new capture session
+        let captureSession = AVCaptureSession()
+        
+        // search for available capture devices
+        let availableDevices = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: AVMediaType.video, position: .back).devices
+        
+        // get capture device, add device input to capture session
+        do {
+            if let captureDevice = availableDevices.first {
+                captureSession.addInput(try AVCaptureDeviceInput(device: captureDevice))
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        // setup output, add output to capture session
+        let captureOutput = AVCaptureVideoDataOutput()
+        captureSession.addOutput(captureOutput)
+        
+        captureOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = view.frame
+        view.layer.addSublayer(previewLayer)
+        
+        captureSession.startRunning()
     }
-
-
+    
+    // called everytime a frame is captured
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        
+        if switcher == 1 {
+            
+            guard let model = try? VNCoreMLModel(for: Resnet50().model) else { return }
+            let request = VNCoreMLRequest(model: model) { (finishedRequest, error) in
+                guard let results = finishedRequest.results as? [VNClassificationObservation] else { return }
+                guard let Observation = results.first else { return }
+                
+                DispatchQueue.main.async(execute: {
+                    self.objectLabel.text = "\(Observation.identifier)"
+                })
+            }
+            guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+            
+            // executes request
+            try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+            
+        }
+        else {
+            
+            
+        }
+        
+    }
+    
 }
-
